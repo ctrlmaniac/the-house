d35055e5047c0ba49e1e4d78de5db1931e1da551
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-18 15:54:12 +0200

    refactor: move the play function in play.py

--- thehouse/__main__.py
+++ thehouse/__main__.py
@@ -1,22 +1,5 @@
-from characters import Player
-from helpers import print_pause, validate_input
-
-from thehouse import TheHouse
-
-
-def play():
-    player = Player()
-    thehouse = TheHouse(player)
-
-    thehouse.play_game()
-
-    print_pause("Do you want to play again?")
-    choice = validate_input("Type yes or no: ", ["yes", "no"])
-
-    if choice == "yes":
-        play()
-    else:
-        quit()
+from __future__ import absolute_import
+from play import play
 
 
 play()
