ce3d0e2e382b03253afb0d8c0f74ec7305935e40
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-18 11:18:35 +0200

    fix: move to the center of the room only if player is still alive

--- thehouse/rooms/studio.py
+++ thehouse/rooms/studio.py
@@ -8,9 +8,35 @@ class Studio:
         self.player = player
         self.lights = random.choice([True, False])
 
+    def prompt_light(self):
+        switch_position = random.choice(["right", "left", "forward", "backward"])
+
+        print_pause("You hear something lurking in the dark")
+        print_pause("You defenetly want to turn the lights on.")
+        print_pause("You extend your arms to:")
+        print_pause("- the right")
+        print_pause("- the left")
+        print_pause("- forward")
+        print_pause("- backward")
+
+        while self.player.health > 0:
+            choice = validate_input(
+                "Type right, left, forward or backward: ",
+                ["right", "left", "backward", "forward"],
+            )
+
+            if choice == switch_position:
+                print_pause("You've turned the lights on!")
+                break
+            else:
+                print_pause("There's nothing here!")
+                print_pause(
+                    "But you sense there's something " "that want to reach your hand..."
+                )
+                self.player.loose_health()
+
     def intro(self):
         lights = "on" if self.lights else "off"
-        switch_position = random.choice(["right", "left", "forward", "backward"])
 
         print_pause("You open your eyes and find yourself lying on the floor")
         print_pause("Your head is pounding and it hurts")
@@ -18,32 +44,25 @@ class Studio:
         print_pause(f"You find yourself in a room with the lights {lights}")
 
         if not self.lights:
-            print_pause("You hear something lurking in the dark")
-            print_pause("You defenetly want to turn the lights on.")
-            print_pause("You extend your arms to:")
-            print_pause("- the right")
-            print_pause("- the left")
-            print_pause("- forward")
-            print_pause("- backward")
+            self.prompt_light()
 
-            while self.player.health > 0:
-                choice = validate_input(
-                    "Type right, left, forward or backward: ",
-                    ["right", "left", "backward", "forward"],
-                )
-
-                if choice == switch_position:
-                    print_pause("You've turned the lights on!")
-                    self.center()
-                else:
-                    print_pause("There's nothing here!")
-                    print_pause(
-                        "But you sense there's something "
-                        "that want to reach your hand..."
-                    )
-                    self.player.loose_health()
-        else:
+        if self.player.is_alive():
             self.center()
+
+    def move(self):
+        choice = validate_input(
+            'Type "right", "left", "forward" or "backward": ',
+            ["right", "left", "forward", "backward"],
+        )
+
+        if choice == "right":
+            self.right()
+        elif choice == "left":
+            self.left()
+        elif choice == "backward":
+            self.backward()
+        elif choice == "forward":
+            self.forward()
 
     def center(self):
         print_pause("You're in the middle of a studio")
@@ -52,4 +71,17 @@ class Studio:
         print_pause("On your right there's a window.")
         print_pause("In front of you there's a closed door.")
         print_pause("What are you gonna do?")
-        quit()
+
+        self.move()
+
+    def right(self):
+        print("the right")
+
+    def left(self):
+        print("left")
+
+    def backward(self):
+        print("back")
+
+    def forward(self):
+        print("forward")
