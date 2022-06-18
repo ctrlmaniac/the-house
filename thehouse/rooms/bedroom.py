9307c8150989f7cd788b088051bd5e953b1de327
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-18 19:58:56 +0200

    feat(bedroom): add pick a drawer

--- thehouse/rooms/bedroom.py
+++ thehouse/rooms/bedroom.py
@@ -1,8 +1,13 @@
 from helpers import print_pause, validate_input
 from .room import Room
+import random
 
 
 class Bedroom(Room):
+    def __init__(self, player, thehouse):
+        super().__init__(player, thehouse)
+        self.key_in_drawer = random.randint(1, 5)
+
     def center(self):
         print_pause("You're in the bedroom!")
         print_pause("On your right there's a window.")
@@ -48,3 +53,46 @@ class Bedroom(Room):
         print_pause("You go back.")
 
         self.move()
+
+    """ FORWARD """
+
+    def forward(self):
+        print_pause("There are five drawers")
+        print_pause("1. open the first")
+        print_pause("2. open the second")
+        print_pause("3. open the third")
+        print_pause("4. open the fourth")
+        print_pause("5. open the fifth")
+
+        self.pick_a_drawer()
+
+    def pick_a_drawer(self):
+        print(self.key_in_drawer)
+
+        choice = validate_input(
+            "Type a number between 1 and 5 included, or back: ",
+            ["1", "2", "3", "4", "5", "back"],
+        )
+
+        if choice == "back":
+            print_pause("You go back to the center of the room.")
+            self.move()
+        else:
+            print_pause(
+                "There's some clothes in it. You dig into them to find something..."
+            )
+
+            if int(choice) == self.key_in_drawer:
+                if "thehouse_key_1" in self.player.items:
+                    print_pause("You've already picked a key from this drawer!")
+                    print_pause("There's nothing more in it.")
+                else:
+                    print_pause("You've found a key!")
+                    self.player.pick_an_item("thehouse_key_1")
+            else:
+                print_pause("There's nothing between the clothes.")
+                print_pause("Something from the outside is moving...")
+                self.player.loose_health()
+
+        if self.player.is_alive():
+            self.pick_a_drawer()
