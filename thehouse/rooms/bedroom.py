efc29a1f798d5623a6132f8d3b2e08b263079911
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-18 19:03:52 +0200

    feat(bedroom): add the backword content

--- thehouse/rooms/bedroom.py
+++ thehouse/rooms/bedroom.py
@@ -1,4 +1,4 @@
-from helpers.print_pause import print_pause
+from helpers.print_pause import print_pause, validate_input
 from .room import Room
 
 
@@ -9,5 +9,21 @@ class Bedroom(Room):
         print_pause("On your left there's a door")
         print_pause("Backwards there's a bed.")
         print_pause("In front of you there's a dresser.")
+
+        self.move()
+
+    """ BACKWARD """
+
+    def backward(self):
+        print_pause("You look tired.")
+        print_pause("Do you want to rest a little bit?")
+
+        choice = validate_input("Type yes or no", ["yes", "no"])
+
+        if choice == "yes":
+            print_pause("You've decided to rest.", 5)
+            self.player.restore_health()
+        else:
+            print_pause("You go back.")
 
         self.move()
