e41fe2b00e00ba51d3d2828739ee867ea516e891
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-18 17:00:35 +0200

    feat(hallway): add more content

--- thehouse/rooms/hallway.py
+++ thehouse/rooms/hallway.py
@@ -4,3 +4,8 @@ from helpers import print_pause
 class Hallway:
     def center(self):
         print_pause("You are in the hallway.")
+        print_pause("Behind you there's a door.")
+        print_pause("On your left there's a door and two paintings.")
+        print_pause("On your right there's a little table and a door.")
+        print_pause("In front of you there's the hall of the house.")
+        print_pause("Whare do you want to go?")
