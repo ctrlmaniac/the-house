f4dedf22664be1d9b7a72096f379a10fcfeaad4d
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-18 22:59:08 +0200

    fix(thehouse): better intro

--- thehouse/thehouse.py
+++ thehouse/thehouse.py
@@ -23,9 +23,13 @@ class TheHouse:
         while not self.player.escaped or self.player.is_alive():
             print_pause("\n\n=== THE HOUSE ===\n\n", 3)
 
-            print_pause("You open your eyes and find yourself lying on the floor")
-            print_pause("Your head is pounding and it hurts")
-            print_pause("With a lot of effort you stand up")
+            print_pause("Someone, or something hit you and you faint.")
+            print_pause(
+                "You hear that this someone or something drags you to someplace.\n", 3
+            )
+            print_pause("You open your eyes and find yourself lying on the floor.")
+            print_pause("Your head is pounding and it hurts.")
+            print_pause("With a lot of effort you stand up.")
 
             self.rooms[self.current_room].center()
 
