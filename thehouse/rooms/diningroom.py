0bd31ac4b792ed8a615dbcc98302b499d1ebe3b6
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-19 10:15:42 +0200

    feat(diningroom): add content to the center method

--- thehouse/rooms/diningroom.py
+++ thehouse/rooms/diningroom.py
@@ -1,8 +1,17 @@
 from .room import Room
 
+from helpers import print_pause
+
 
 class Diningroom(Room):
     def center(self):
-        print("You're in the dining room!")
+        print_pause("You're in the dining room!")
+        print_pause("There's a bloody mess here...")
+        print_pause("Someone or something has killed three poeple!")
+        print_pause("Forward there's a corpse!")
+        print_pause("On your left there's another corpse!")
+        print_pause("On your back there's a third corpse!")
+        print_pause("On your left there's a door.")
+        print_pause("Maybe you can check the pockets of the corpse!")
 
         self.move()
