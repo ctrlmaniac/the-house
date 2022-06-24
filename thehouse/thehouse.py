aea89ef75e11b0f50a84a764e0242bb4c7670d36
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-24 12:30:13 +0200

    refactor: init thehouse class

---
+++ thehouse/thehouse.py
@@ -0,0 +1,14 @@
+"""thehouse.
+
+This class wraps all rooms and play the game
+"""
+
+
+class TheHouse:
+    """TheHouse."""
+
+    def __init__(self):
+        pass
+
+    def play(self):
+        pass
