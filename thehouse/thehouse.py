eabe7eec69a33f3c8be34a9dd28537764c0c3385
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-24 13:09:43 +0200

    refactor: add intro of thehouse

--- thehouse/thehouse.py
+++ thehouse/thehouse.py
@@ -14,4 +14,12 @@ class TheHouse:
 
     def play(self):
         """Play engine."""
-        print_pause("Ciao!")
+        print_pause("\n\n=== THE HOUSE ===\n\n", 3)
+
+        print_pause("Someone, or something hit you and you faint.")
+        print_pause(
+            "You hear that this someone or something drags you to someplace.\n", 3
+        )
+        print_pause("You open your eyes and find yourself lying on the floor.")
+        print_pause("Your head is pounding and it hurts.")
+        print_pause("With a lot of effort you stand up.")
